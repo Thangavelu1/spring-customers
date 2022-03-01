b95a47f9cb9d4e28a5167666cc724bbb76778cff
Thangavelu <thangavelu706@gmail.com>
2022-03-01 16:55:43 +0530

    Spring Customer Project

---
+++ src/main/java/com/hexaware/HexaCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaCustomersApplication.class, args);
+	}
+
+}
